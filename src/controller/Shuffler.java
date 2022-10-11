2e90c733308819c4987a5a6c4ee85f84c7efdbdd
Christopher-Nottingham <cnott729@mtroyal.ca>
2022-10-11 15:58:48 -0700

    Fixed all deck errors

--- src/controller/Shuffler.java
+++ src/controller/Shuffler.java
@@ -32,114 +32,28 @@ public class Shuffler extends CardDeck {
     public Card current;
     
 
-    public ArrayList <Card> deck1 = new ArrayList<Card>(); 
+//    public ArrayList <Card> deck1 = new ArrayList<Card>(); 
 
-    private CardDeck shuffledDeck = new CardDeck();
+    //private CardDeck shuffledDeck = new CardDeck();
     
     
-
-    
-    
-    public void toArrayList() {
-    	
-    }
-    
-    
-    public void swap (Card a, Card b) {
-    	
-    	
-    	    	//Collections.swap(cardDeck, a, b);
-    	
-    	
-    	
-    	
-    	
-	}
-    
-    
-    
-	
-
-
-
-
-
-		
-
-
 	public void shuffle(){
 
 		for (int i=0; i<52; i++) {
 			int card = (int) (Math.random() * (52-i));
 			int card1 = (int) (Math.random() * (52-i));		
-			Collections.swap(shuffledDeck.deck, card, card1);	
+			Collections.swap( deck, card, card1);	
 		}
 		
 
-		System.out.println(shuffledDeck.deck.get(0) + "\n" + shuffledDeck.deck.get(1) + "\n" + shuffledDeck.deck.get(2) + "\n" + shuffledDeck.deck.get(3) + "\n" + shuffledDeck.deck.get(4));
-
-
-
-//		CardDeck shuffledDeck = new CardDeck();
-
-		
-		
-//		shuffledDeck.createDeck();
-		
-	//	tail = shuffledDeck.linkedListCards.getTail();
-	//	head = shuffledDeck.linkedListCards.getHead();
-		
-//		shuffledDeck.printCardDeck();
-		
-		
-		
-
-		
-		//System.out.println(head.item);
-		
-//		System.out.println("/n/n/n");
-//		shuffledDeck.removeEndNode();
-//		shuffledDeck.removeEndNode();
-//		shuffledDeck.removeEndNode();
-
-//		
-//	 shuffledDeck = new CardDeck();
-//		
-//		
-//		
-//		shuffledDeck.createDeck();
-//		
-//	//	tail = shuffledDeck.linkedListCards.getTail();
-//	//	head = shuffledDeck.linkedListCards.getHead();
-//		
-
-//		shuffledDeck.printCardDeck();
-
-//	
-		
-
-//		
-//		
-//		
-//		
-//		//System.out.println(head.item);
-//		
-//		System.out.println("/n/n/n");
-////		shuffledDeck.removeEndNode();
-////		shuffledDeck.removeEndNode();
-////		shuffledDeck.removeEndNode();
-////		shuffledDeck.printCardDeck();
-////	
-//		
-
-	        
+		System.out.println(deck.get(0) + "\n" + deck.get(1) + "\n" + deck.get(2) + "\n" + deck.get(3) + "\n" + deck.get(4));        
 
 }
 
 
 	@Override
 	public String toString() {
-		return "Shuffler [shuffledDeck=" + shuffledDeck + "]";
+		return "Shuffler [shuffledDeck=" + deck + "]";
 	}
 
 
